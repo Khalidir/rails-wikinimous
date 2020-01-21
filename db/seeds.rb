604a9dc8adbb8786928f9c3e7b8f30f8ad0a3bab
Rachid Khalidi rkhal@yahoo.fr <rkhal@yahoo.fr>
2020-01-21 10:48:26 +0000

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Name.name, content: Faker::ChuckNorris.fact)
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
